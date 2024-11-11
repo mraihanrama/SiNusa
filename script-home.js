5060a39efbd7a84b0ed4fa5d26a2ec7a4fa41b2f
Rai Rama <147982945+mraihanrama@users.noreply.github.com>
2024-11-11 20:57:03 +0700

    Create script-home.js

---
+++ script-home.js
@@ -0,0 +1,7 @@
+function goToPage(choice) {
+    if (choice === 'musik') {
+        window.location.href = 'musik.html'; // Halaman alat musik
+    } else if (choice === 'tarian') {
+        window.location.href = 'tarian.html'; // Halaman tari tradisional
+    }
+}
